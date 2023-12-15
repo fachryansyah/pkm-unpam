1b46055c77fc5a0fbeaa3572d047cffa0565f3b6
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2023-12-15 14:05:27 +0700

    change port deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'pkm',
-      script: 'serve src -n -p 6000', // Path to the compiled main.js file
+      script: 'serve src -n -p 1000', // Path to the compiled main.js file
       instances: 1, // Number of instances to run
       autorestart: true, // Automatically restart the app if it crashes
       watch: false, // Set to true to enable file watching and automatic restart on file changes
